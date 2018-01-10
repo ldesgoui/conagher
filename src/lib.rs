0cafe2f678ffa7dc4830ea6a8e33f0b5c29dd927
ldesgoui <ldesgoui@gmail.com>
2018-01-10 16:00:02 +0100

    :100: + 125t works

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 #![allow(unused_attributes)]
 #![allow(non_upper_case_globals)]
+#![allow(non_snake_case)]
+#![feature(concat_idents)]
 #![feature(link_args)]
 #![feature(pointer_methods)]
 #![link_args = "-Wl,-z,defs -Wl,--no-undefined -ldl"]
 
 extern crate badlog;
+extern crate cpp_demangle;
 #[macro_use]
 extern crate detour;
 extern crate goblin;
@@ -13,39 +16,64 @@ extern crate lazy_static;
 extern crate libc;
 #[macro_use]
 extern crate log;
-extern crate region;
 
-use region::Protection;
-use goblin::elf::Elf;
 use detour::Detour;
+use detour::StaticDetour;
+use goblin::elf::Elf;
+use std::collections::HashMap;
 use std::ffi::CStr;
-use std::path::PathBuf;
 use std::fs::File;
 use std::io::Read;
-use std::collections::HashMap;
+use std::path::PathBuf;
 use std::sync::Mutex;
 
-lazy_static! {
-    static ref DLOPEN: fn(*const i8, i32) -> *mut u8 = unsafe {
-        badlog::init_from_env("TF2RS_LOG"); // bit hacky but trustable
-        std::mem::transmute(libc::dlsym(libc::RTLD_NEXT, "dlopen".as_ptr() as *const i8))
-    };
-
-    static ref SYMBOLS: Mutex<HashMap<String, goblin::elf::sym::Sym>> =
-        Mutex::new(HashMap::new());
-
-    // static ref DETOURS: Mutex<Vec<RawDetour>> = Mutex::new(Vec::new());
+static_detours! {
+    struct CServerGameDLL_DLLInit: fn(*const (), *const (), *const (), *const (), *const ()) -> i8;
+    struct CServerGameDLL_GetTickInterval: fn(*const ()) -> f32;
 }
 
-const CServerGameDLL_DLLInit: &'static str =
-    "_ZN14CServerGameDLL7DLLInitEPFPvPKcPiES5_S5_P11CGlobalVars";
+lazy_static! {
+    static ref DLOPEN: fn(*const i8, i32) -> *const i8 = {
+        badlog::init_from_env("TF2RS_LOG"); // bit hacky but trustable
+        unsafe { std::mem::transmute(libc::dlsym(libc::RTLD_NEXT, "dlopen".as_ptr() as *const i8)) }
+    };
 
-static_detours! {
-    struct DLLInit: extern "C" fn(*const (), *const (), *const (), *const ()) -> i8;
+    static ref SYMBOLS: Mutex<HashMap<String, usize>> = Mutex::new(HashMap::new());
+
+    static ref detour_CServerGameDLL_DLLInit: Mutex<
+        StaticDetour<fn(*const (), *const (), *const (), *const (), *const ()) -> i8>,
+    > = Mutex::new(unsafe {
+        info!("Applying detour to: {:?}", cpp_demangle::Symbol::new("_ZN14CServerGameDLL7DLLInitEPFPvPKcPiES5_S5_P11CGlobalVars").unwrap().to_string());
+        CServerGameDLL_DLLInit
+            .initialize(
+                std::mem::transmute(
+                    symbol("_ZN14CServerGameDLL7DLLInitEPFPvPKcPiES5_S5_P11CGlobalVars")
+                        .expect("CServerGameDLL_DLLInit detour initialized with no symbol available"),
+                ),
+                |this, a, b, c, d| {
+                    info!("We're in business.");
+                    CServerGameDLL_DLLInit.get().unwrap().call(this, a, b, c, d)
+                },
+            )
+            .expect("Failed to initialize DLLInit detour")
+    });
+
+    static ref detour_CServerGameDLL_GetTickInterval: Mutex<StaticDetour<fn(*const ()) -> f32>> =
+        Mutex::new(unsafe {
+        info!("Applying detour to: {:?}", cpp_demangle::Symbol::new("_ZNK14CServerGameDLL15GetTickIntervalEv").unwrap().to_string());
+            CServerGameDLL_GetTickInterval
+                .initialize(
+                    std::mem::transmute(symbol("_ZNK14CServerGameDLL15GetTickIntervalEv").unwrap()),
+                    |_| {
+                        0.008
+                    },
+                )
+                .unwrap()
+        });
 }
 
 #[no_mangle]
-pub extern "C" fn dlopen(filename: *const i8, flags: i32) -> *mut u8 {
+pub extern "C" fn dlopen(filename: *const i8, flags: i32) -> *const i8 {
     let handle = (DLOPEN)(filename, flags);
     if handle.is_null() {
         return handle;
@@ -53,51 +81,66 @@ pub extern "C" fn dlopen(filename: *const i8, flags: i32) -> *mut u8 {
 
     let mut path = PathBuf::from("bin");
     path.push(unsafe { CStr::from_ptr(filename) }.to_str().unwrap());
-    if !path.is_file() {
-        warn!("dlopen-ed but inexistant: {}", path.display());
+    // TODO
+    if path.file_name().unwrap() != "server_srv.so" {
+        debug!(
+            "dlopen: skipping uninteresting {:?}",
+            path.file_name().unwrap()
+        );
+        return handle;
+    } else if !path.is_file() {
+        warn!("dlopen of inexistant: {}", path.display());
         return handle;
     }
 
-    info!("processing: {:?}", path.file_name().unwrap());
     let mut buffer = Vec::new();
-    let mut fd = File::open(path).unwrap();
+    let mut fd = File::open(path.as_path()).unwrap();
     fd.read_to_end(&mut buffer).unwrap();
 
-    let elf: Elf = Elf::parse(&buffer).unwrap();
+    let library_addr: usize = unsafe { *(handle as *const *const i8) as usize };
 
-    let mut map = SYMBOLS.lock().unwrap();
-
-    elf.syms
-        .iter()
-        .map(|sym| map.insert(elf.strtab.get_unsafe(sym.st_name).unwrap().to_string(), sym))
-        .last();
-    debug!("{:?} symbols registered", map.len());
-
-    match map.get(CServerGameDLL_DLLInit) {
-        None => debug!("not found"),
-        Some(sym) => unsafe {
-            let ptr = (*(handle as *mut *mut u8)).add(sym.st_value as usize);
-            region::protect(ptr, sym.st_size as usize, Protection::ReadWriteExecute).expect("rwx");
-            let mut hook = DLLInit
-                .initialize(std::mem::transmute(ptr), |_, _, _, _| {
-                    debug!("in DLLInit");
-                    0
+    Elf::parse(&buffer)
+        .map(|elf| {
+            info!("dlopen: processing {:?}", path.file_name().unwrap());
+            let mut map = SYMBOLS.try_lock().unwrap();
+            elf.syms
+                .iter()
+                .map(|sym| {
+                    map.insert(
+                        elf.strtab.get_unsafe(sym.st_name).unwrap().to_string(),
+                        library_addr + sym.st_value as usize,
+                    )
                 })
-                .expect("help");
-            region::protect(ptr, sym.st_size as usize, Protection::ReadExecute).expect("rx");
-            hook.enable().expect("help2");
-            let func: fn(*const (), *const (), *const (), *const ()) -> u8 =
-                std::mem::transmute(ptr);
-            (func)(
-                std::ptr::null(),
-                std::ptr::null(),
-                std::ptr::null(),
-                std::ptr::null(),
-            );
+                .last();
+        })
+        .unwrap_or_else(|_| {
+            warn!(
+                "dlopen: failed to parse ELF32 for {:?}",
+                path.file_name().unwrap()
+            )
+        });
 
-            // DETOURS.lock().unwrap().push(hook);
-        },
+    unsafe {
+        // TODO: safety (could crash if server_srv.so isnt loaded)
+        detour_CServerGameDLL_DLLInit
+            .try_lock()
+            .unwrap()
+            .enable()
+            .unwrap();
+        detour_CServerGameDLL_GetTickInterval
+            .try_lock()
+            .unwrap()
+            .enable()
+            .unwrap();
     };
 
     handle
+}
+
+fn symbol(name: &str) -> Option<*const ()> {
+    SYMBOLS
+        .lock()
+        .unwrap()
+        .get(name)
+        .map(|&sym| unsafe { std::mem::transmute(sym) })
 }
