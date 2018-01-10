4239ce49ce9ab457d5ca27cb0ed8aacd17194809
ldesgoui <ldesgoui@gmail.com>
2018-01-10 09:31:51 +0100

    issue: StaticDetour de-initializes on RAII

--- src/lib.rs
+++ src/lib.rs
@@ -23,12 +23,18 @@ use std::path::PathBuf;
 use std::fs::File;
 use std::io::Read;
 use std::collections::HashMap;
+use std::sync::Mutex;
 
 lazy_static! {
     static ref DLOPEN: fn(*const i8, i32) -> *mut u8 = unsafe {
         badlog::init_from_env("TF2RS_LOG"); // bit hacky but trustable
         std::mem::transmute(libc::dlsym(libc::RTLD_NEXT, "dlopen".as_ptr() as *const i8))
     };
+
+    static ref SYMBOLS: Mutex<HashMap<String, goblin::elf::sym::Sym>> =
+        Mutex::new(HashMap::new());
+
+    // static ref DETOURS: Mutex<Vec<RawDetour>> = Mutex::new(Vec::new());
 }
 
 const CServerGameDLL_DLLInit: &'static str =
@@ -57,25 +63,39 @@ pub extern "C" fn dlopen(filename: *const i8, flags: i32) -> *mut u8 {
     let mut fd = File::open(path).unwrap();
     fd.read_to_end(&mut buffer).unwrap();
 
-    let elf = Elf::parse(&buffer).unwrap();
-    let map: HashMap<&str, goblin::elf::sym::Sym> = elf.syms
+    let elf: Elf = Elf::parse(&buffer).unwrap();
+
+    let mut map = SYMBOLS.lock().unwrap();
+
+    elf.syms
         .iter()
-        .map(|sym| (elf.strtab.get(sym.st_name).unwrap().unwrap(), sym))
-        .collect();
+        .map(|sym| map.insert(elf.strtab.get_unsafe(sym.st_name).unwrap().to_string(), sym))
+        .last();
+    debug!("{:?} symbols registered", map.len());
 
     match map.get(CServerGameDLL_DLLInit) {
-        None => (),
+        None => debug!("not found"),
         Some(sym) => unsafe {
-            let base = *(handle as *mut *mut u8);
-            let ptr = base.add(sym.st_value as usize);
+            let ptr = (*(handle as *mut *mut u8)).add(sym.st_value as usize);
             region::protect(ptr, sym.st_size as usize, Protection::ReadWriteExecute).expect("rwx");
             let mut hook = DLLInit
-                .initialize(std::mem::transmute(ptr), |a, b, c, d| {
-                    error!("WE IN BOYS AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-                    DLLInit.get().expect("idfk").call(a, b, c, d)
+                .initialize(std::mem::transmute(ptr), |_, _, _, _| {
+                    debug!("in DLLInit");
+                    0
                 })
                 .expect("help");
+            region::protect(ptr, sym.st_size as usize, Protection::ReadExecute).expect("rx");
             hook.enable().expect("help2");
+            let func: fn(*const (), *const (), *const (), *const ()) -> u8 =
+                std::mem::transmute(ptr);
+            (func)(
+                std::ptr::null(),
+                std::ptr::null(),
+                std::ptr::null(),
+                std::ptr::null(),
+            );
+
+            // DETOURS.lock().unwrap().push(hook);
         },
     };
 
